973c21d8866fc0d40f8b14b3dbb78a21f5f883db
Siddhant Negi <snegi@Cogitate.us>
2023-07-19 22:19:02 +0530

    Adding footer and installing font-aweasome in the project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [showAbout] = useState(true);
+  const [showHelp] = useState(true);
 
-export default Footer
+
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white' }}>
+      <div className="footer-content">
+        <br></br>
+        <br></br>
+        <div className="footer-links" style={{ display: 'flex', paddingLeft: '100px' }}>
+          <div className="footer-section" style={{ marginRight: '10px' }}>
+            <h2 style={{ fontSize: '1rem' }}>About</h2>
+            {showAbout && (
+              <div>
+                <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Our Values</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Privacy Policy</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Terms & Conditions</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Corporate Information</Link></li>
+                </ul>
+              </div>
+
+            )}
+            <br></br>
+
+          </div>
+
+          <br></br>
+          <div className="footer-section" style={{ paddingLeft: '30px' }}>
+            <h2 style={{ fontSize: '1rem' }}>Help</h2>
+            {showHelp && (
+              <div>
+                <ul style={{ listStyleType: 'none', paddingLeft: '0' }}>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Knowledge</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>FAQs</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Return & Refund Policy</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Contact Us</Link></li>
+                  <li style={{ paddingLeft: '0px' }}><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Track Order</Link></li>
+                </ul>
+              </div>
+            )}
+            <br></br>
+          </div>
+
+        </div>
+
+
+      </div>
+      <div>
+
+        <hr style={{
+          marginBottom: '0px', border: '0px', height: '0.5px', width: '90%', background: 'rgba(255, 255, 255, 0.3)'
+        }}></hr>
+
+        <br></br>
+        <div style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginLeft: '6.2rem' , }}>
+          <FontAwesomeIcon icon={faFacebook} size="lg" style={{ marginRight: '10px', color: 'rgba(255, 255, 255, 0.6)' }} />
+          <FontAwesomeIcon icon={faInstagram} size="lg" style={{ marginRight: '10px', color:'rgba(255, 255, 255, 0.6)' }} />
+        </div>
+        <div style={{ paddingLeft: '100px',color: 'rgba(255, 255, 255, 0.6)' }}>
+          <p> Copyright &copy;  {new Date().getFullYear()}, Minimalist.</p>
+          <p>Powered by Shopify</p>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
+    <header style={{ backgroundColor: 'black', color: 'white' }}>
+      <div>
       <ul>
         <Link to='/'><li>Home</li></Link>
         <Link to='/login'><li>Login</li></Link>
         <Link to='/signup'><li>Signup</li></Link>
       </ul>
     </div>
+      </header>
+
+    
   )
 }
 
